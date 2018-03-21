806c8987863404ee2b376cd72f09dd059d31eb15
kouhei.takemoto <take64@citrus.tk>
2018-03-21 22:34:15 +0900

    Smarty拡張:empty_defaultの作成

---
+++ library/smarty3_plugins/modifier.empty_default.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @copyright   Copyright 2017, Citrus/besidesplus All Rights Reserved.
+ * @author      take64 <take64@citrus.tk>
+ * @license     http://www.citrus.tk/
+ */
+
+
+/**
+ * empty評価される場合のデフォルト表示
+ *
+ * @param mixed  $value   評価変数
+ * @param string $default デフォルト内容
+ * @return string
+ */
+function smarty_modifier_empty_default($value, $default = '')
+{
+    if (empty($value) !== true)
+    {
+        return $value;
+    }
+
+    return $default;
+}
