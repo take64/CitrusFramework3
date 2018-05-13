9e7bb3aa42499d2f84e303b54688895982f72a5c
kouhei.takemoto <take64@citrus.tk>
2018-05-13 16:03:11 +0900

    AWSライブラリ:cloudwatchlogsライブラリの作成

---
+++ Aws.class.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @copyright   Copyright 2018, Citrus/besidesplus All Rights Reserved.
+ * @author      take64 <take64@citrus.tk>
+ * @license     http://www.citrus.tk/
+ */
+
+namespace Citrus;
+
+
+class CitrusAws
+{
+    /** CitrusConfigureキー */
+    const CONFIGURE_KEY = 'aws';
+
+
+
+    /** @var bool */
+    private static $IS_INITIALIZED = false;
+
+
+
+    /**
+     * initialize
+     */
+    public static function initialize()
+    {
+        // is initialize
+        if (self::$IS_INITIALIZED === true)
+        {
+            return ;
+        }
+
+        // configure
+        $configure = CitrusConfigure::configureMerge(self::CONFIGURE_KEY);
+
+        // require
+        require_once $configure['path'];
+
+        // initialize
+        self::$IS_INITIALIZED = true;
+    }
+}
