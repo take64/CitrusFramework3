cc768683c3ef70178bd72748b6d0751ebe9a2454
kouhei.takemoto <take64@citrus.tk>
2019-11-09 22:56:23 +0900

    リファクタリング：auth

--- src/Authentication/Item.class.php
+++ src/Authentication/Item.class.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 /**
  * @copyright   Copyright 2017, CitrusFramework. All Rights Reserved.
  * @author      take64 <take64@citrus.tk>
@@ -9,12 +12,15 @@ namespace Citrus\Authentication;
 
 use Citrus\Database\Column;
 
+/**
+ * 認証アイテム
+ */
 class Item extends Column
 {
     /** @var string user id */
     public $user_id;
 
-    /** @var string password */
+    /** @var string|null password */
     public $password;
 
     /** @var string token */
