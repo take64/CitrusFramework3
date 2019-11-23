bb1cc47844cfc419e5fbd231e394d326ada5913c
kouhei.takemoto <take64@citrus.tk>
2019-11-24 01:02:54 +0900

    コードスタイルの修正

--- src/Formmap/Element.class.php
+++ src/Formmap/Element.class.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 /**
  * @copyright   Copyright 2017, CitrusFramework. All Rights Reserved.
  * @author      take64 <take64@citrus.tk>
@@ -7,13 +10,15 @@
 
 namespace Citrus\Formmap;
 
-
 use Citrus\CitrusException;
 use Citrus\Formmap;
 use Citrus\Message;
 use Citrus\NVL;
 use Citrus\Struct;
 
+/**
+ * フォームエレメント
+ */
 class Element extends Struct
 {
     /** var type int */
