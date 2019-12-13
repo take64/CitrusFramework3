8667a55df58a7f96eb4a5ca88549d21df1383868
kouhei.takemoto <take64@citrus.tk>
2019-12-14 01:33:41 +0900

    Slackに新しいConfigureを適用

--- src/Integration/Slack/Attachments.class.php
+++ src/Integration/Slack/Attachments.class.php
@@ -1,16 +1,21 @@
 <?php
+
+declare(strict_types=1);
+
 /**
  * @copyright   Copyright 2017, CitrusFramework. All Rights Reserved.
  * @author      take64 <take64@citrus.tk>
  * @license     http://www.citrus.tk/
  */
 
-namespace Citrus\Slack;
+namespace Citrus\Integration\Slack;
 
-use Citrus\Struct;
+use Citrus\Variable\Structs;
 
-class Attachments extends Struct implements Item
+class Attachments
 {
+    use Structs;
+
     /** @var string fallback */
     public $fallback = '';
 
